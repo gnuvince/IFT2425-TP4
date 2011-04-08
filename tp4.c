7026a4f1b5af77e14c83fd6f06ff783a21c031ce
Vincent Foley <vfoley@gmail.com>
2011-04-07 20:30:03 -0400

    Started adding plotting

--- tp4.c
+++ tp4.c
@@ -8,6 +8,19 @@
 #define END   150
 
 
+#define NAME_IMG_OUT "VincentEric.pgm"
+#define NBCHAR 200
+
+#define WIDTH  512
+#define LENGTH 512
+
+#define MID_WDTH WIDTH/2
+#define MID_LGTH LENGTH/2
+
+#define WHITE 255
+#define BLACK 0
+
+
 
 static double e = 0;
 static double a = 0;
@@ -23,14 +36,80 @@ static double w = 0;
   y''(t) = y'(t)(e - y²(t)) - y(t) + a*cos(wt)
 
   Avec:
-    y(0)  = 0
-    y'(0) = 0
+  y(0)  = 0
+  y'(0) = 0
 
   Posons: x(t) = y'(t):
 
-    x'(t) = x(t)(e - y²(t)) - y(t) + a*cos(wt) = f1(t, y(t), x(t))
-    y'(t) = x(t)                               = f2(t, y(t), x(t))
- */
+  x'(t) = x(t)(e - y²(t)) - y(t) + a*cos(wt) = f1(t, y(t), x(t))
+  y'(t) = x(t)                               = f2(t, y(t), x(t))
+*/
+
+
+double** fmatrix_allocate_2d(int vsize,int hsize)
+{
+    int i;
+    double** matrix;
+    double *imptr;
+
+    matrix=(double**)malloc(sizeof(double*)*vsize);
+    if (matrix==NULL) printf("probleme d'allocation memoire");
+
+    imptr=(double*)malloc(sizeof(double)*hsize*vsize);
+    if (imptr==NULL) printf("probleme d'allocation memoire");
+
+    for(i=0;i<vsize;i++,imptr+=hsize) matrix[i]=imptr;
+    return matrix;
+}
+
+//----------------------------------------------------------*/
+/* Libere la memoire de la matrice 2d de double              */
+/*----------------------------------------------------------*/
+void free_fmatrix_2d(double** pmat)
+{
+    free(pmat[0]);
+    free(pmat);
+}
+
+/*----------------------------------------------------------*/
+/* Sauvegarde de l'image de nom <name> au format pgm        */
+/*----------------------------------------------------------*/
+void SaveImagePgm(char* name,double** mat,int length,int width)
+{
+    int i,j;
+    FILE* fic;
+
+
+    /*--ouverture fichier--*/
+    fic=fopen(NAME_IMG_OUT, "w");
+    if (fic==NULL)
+    { printf(" Probleme dans la sauvegarde de %s",NAME_IMG_OUT);
+        exit(-1); }
+    printf("\n Sauvegarde de %s au format pgm\n",name);
+
+    /*--sauvegarde de l'entete--*/
+    fprintf(fic,"P5");
+    fprintf(fic,"\n# IMG Module");
+    fprintf(fic,"\n %d %d",length,width);
+    fprintf(fic,"\n255\n");
+
+    /*--enregistrement--*/
+    for(i=0;i<length;i++)
+        for(j=0;j<width;j++)
+            fprintf(fic,"%c",(char)mat[i][j]);
+
+    /*--fermeture fichier--*/
+    fclose(fic);
+}
+
+/*-------------------------------------------------------------*/
+/*--- SetPointBlack -------------------------------------------*/
+/*-------------------------------------------------------------*/
+void SetPointBlack(double** Mat,int lgth,int wdth,int row,int col)
+{
+    Mat[MID_LGTH-row][col+MID_WDTH]=0.0;
+}
+
 
 
 double f1(double t, double yt, double xt) {
@@ -58,6 +137,13 @@ int main(int argc, char *argv[]) {
     double k[2][6];
     double yn = 0.0;
     double xn = 0.0;
+    double **matrice = fmatrix_allocate_2d(WIDTH, LENGTH);
+
+    for (int i = 0; i < WIDTH; ++i) {
+        for (int j = 0; j < LENGTH; ++j) {
+            matrice[i][j] = WHITE;
+        }
+    }
 
     while (tn < END) {
         k[0][0] = H * f1(tn, yn, xn);
@@ -97,12 +183,17 @@ int main(int argc, char *argv[]) {
         double yn1 = yn + k[1][0]*16/135 + k[1][2]*6656/12825 + k[1][3]*28561/56430 - k[1][4]*9/50 + k[1][5]*2/55;
         double xn1 = xn + k[0][0]*16/135 + k[0][2]*6656/12825 + k[0][3]*28561/56430 - k[0][4]*9/50 + k[0][5]*2/55;
 
+        SetPointBlack(matrice, LENGTH, WIDTH, yn1, xn1);
+
         printf("%g %g\n", yn1, xn);
 
         yn = yn1;
         xn = xn1;
         tn += H;
     }
+
+    SaveImagePgm(NAME_IMG_OUT, matrice, LENGTH, WIDTH);
+    free_fmatrix_2d(matrice);
 
     return EXIT_SUCCESS;
 }
